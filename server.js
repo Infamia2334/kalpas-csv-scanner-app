b12717f3c97dea1372fe3fa5d996b5aac4dd208d
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-06-25 01:59:31 +0530

    Added Authentication middleware

--- server.js
+++ server.js
@@ -16,12 +16,40 @@ db.once("open", function(){
     console.log("Connection to mongoDB successful")
 })
 
+//authentication token
+const permitted = process.env.TOKEN
+
+//Authentication middleware
+function authMiddleware(req, res, next){
+    let headers = req.headers
+    if(headers.permission){
+        let permission = headers.permission
+        if(permission == permitted){
+            next()
+        }
+        else{
+            res.status(403).send("You are forbidden")
+        }
+    }
+    else{
+        res.status(401).send("You do not have permission")
+    }
+
+}
+
+app.get("/", function(req, res){
+    res.send("Welcome to the kalpas-API, please refer to the documentation in github repo found in this link : https://github.com/Infamia2334/kalpas-csv-scanner-app, to interact and test the APIs")
+})
+
+//CSV_Scanner API
 const scannerRouter = require("./routes/scanners")
-app.use("/scanner", scannerRouter)
+app.use("/scanner", authMiddleware, scannerRouter)
 
+//CRUD_API
 const crudRouter = require("./routes/cruders")
-app.use("/cruder", crudRouter)
+app.use("/cruder", authMiddleware, crudRouter)
 
+// LISTENING PORT
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
