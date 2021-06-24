b553487395e007e6799eacf747cd068a4f98692d
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-06-24 23:58:16 +0530

    Changed csv file, scanner and crud API complete, making new authentication for crud api

--- routes/cruders.js
+++ routes/cruders.js
@@ -1,4 +1,97 @@
+const express = require("express")
 const mongoose = require('mongoose')
+const router = express.Router()
+
 
 mongoose.connect("mongodb+srv://admin-dipan:Test123@appcluster.x7nob.mongodb.net/kalpasCSVdb?retryWrites=true&w=majority",  {useNewUrlParser : true, useUnifiedTopology: true})
+const BioStats = require("../models/bioStats")
 
+
+//GET ALL DATA IN COLLECTION
+router.get("/", function(req,res){
+    BioStats.find( function(err, found){
+        if(!err){
+            res.send(found)
+        }
+    })
+})
+
+router.get("/getOne", function(req, res){
+    BioStats.findOne({Name: req.query.name}, function(err, found){
+        if(!err){
+            res.send(found)
+        }
+        else{
+            res.send(err)
+        }
+    })
+})
+
+router.post("/", function(req, res){
+    
+    const newBioStats = new BioStats({
+        Name: req.body.name,
+        Sex: req.body.sex,
+        Age: req.body.age,
+        Height:req.body.height,
+        Weight: req.body.weight
+    })
+
+    newBioStats.save(function(err){
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send("New Biostats Created and Saved")
+        }
+    })
+})
+
+
+//to delete everything in collection
+router.delete("/deleteAll", function(req, res){
+    BioStats.deleteMany(function(err){
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send("Successfully deleted all Data in COLLECTION")
+        }
+    })
+})
+
+router.delete("/deleteOne", function(req, res){
+    BioStats.deleteOne({Name: req.body.name} ,function(err){
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send("Successfully deleted said data" )
+        }
+    })
+})
+
+router.put("/", function(req, res){
+    var objForUpdate = {
+        Name: req.body.name,
+        Sex: req.body.sex,
+        Age: req.body.age,
+        Height:req.body.height,
+        Weight: req.body.weight
+    }
+
+    BioStats.findOneAndUpdate({Name: req.query.name}, objForUpdate, function(err, docs){
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.send(docs + " and Updated")
+        }
+    })
+})
+
+
+
+
+
+module.exports = router
--- routes/scanners.js
+++ routes/scanners.js
@@ -6,20 +6,20 @@ const mongoose = require('mongoose')
 
 
 mongoose.connect("mongodb+srv://admin-dipan:Test123@appcluster.x7nob.mongodb.net/kalpasCSVdb?retryWrites=true&w=majority",  {useNewUrlParser : true, useUnifiedTopology: true})
+// mongoose.connect("mongodb://localhost:27017/kalpasCSVdb",  {useNewUrlParser : true, useUnifiedTopology: true})
+// const financialSchema = new mongoose.Schema({
+//     year: String,
+//     industry_code_ANZSIC: String,
+//     industry_name_ANZSIC: String,
+//     rme_size_grp: String,
+//     variable: String,
+//     value: String,
+//     unit: String
+// })
 
-const financialSchema = new mongoose.Schema({
-    year: String,
-    industry_code_ANZSIC: String,
-    industry_name_ANZSIC: String,
-    rme_size_grp: String,
-    variable: String,
-    value: String,
-    unit: String
-})
+// const BioStats =  mongoose.model("BioStats", financialSchema)
 
-const Finance =  mongoose.model("Finances", financialSchema)
-
-
+const BioStats = require("../models/bioStats")
 //getting csv files
 router.get("/", function(req,res){
     res.send("Hello world")
@@ -40,11 +40,11 @@ var upload = multer({storage: storage})
 router.post("/",upload.single("filename"),  function(req,res){
     var flieInfo = req.file
     res.send(flieInfo)
-    // console.log(req.file.path)
+     console.log(req.file)
     csv().fromFile(req.file.path)
     .then(function(jsonObj){
-
-        Finance.insertMany(jsonObj)
+        console.log(jsonObj)
+        BioStats.insertMany(jsonObj)
     })
 
    
